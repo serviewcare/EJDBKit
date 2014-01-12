74ce7223961dfa5e18c2ad2f9acd3fb37f5f102e
John Dragos <janosd@gmail.com>
2014-01-12 21:43:06 +0100

    Added xccheckout

--- EJDBKit/EJDBKit/Source/EJDBQueryBuilderDelegate.h
+++ EJDBKit/EJDBKit/Source/EJDBQueryBuilderDelegate.h
@@ -1,9 +1,19 @@
 #import <Foundation/Foundation.h>
 
+/**
+ This protocol is implemented by both EJDBQueryBuilder and EJDBFIQueryBuilder classes to allow for more convenient query creation in various parts of the framework.
+*/
+
 @protocol EJDBQueryBuilderDelegate <NSObject>
 
+/** 
+ This should return a properly formatted ejdb query dictionary.
+*/
 - (NSDictionary *)query;
 
+/**
+ This should return a properly formatted ejdb hints dictionary.
+*/
 - (NSDictionary *)hints;
 
 @end
